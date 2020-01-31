90dccf40e5e33b74f09bd7e9ca9b106f97706af6
Jonathan A. Gross <jarthurgross@gmail.com>
2020-01-31 16:44:03 -0500

    Add more SDP code

--- src/irrep_codes/sdp.py
+++ src/irrep_codes/sdp.py
@@ -2,8 +2,27 @@ import numpy as np
 import picos as pic
 import cvxopt as cvx
 
-import qinfo as qi
+import pysme.integrate as integ
 import qinfo.supops as supops
+
+def get_error_tensors(Ls, H, times, solve_ivp_kwargs=None):
+    integrator = integ.UncondLindbladIntegrator(Ls, H)
+    error_tensors = get_process_tensors_from_lindblad(integrator, times, solve_ivp_kwargs)
+    return error_tensors
+
+def get_process_tensors_from_lindblad(integrator, times, solve_ivp_kwargs=None):
+    dim = integrator.basis.dim
+    kets = [np.array([1 if j==k else 0 for k in range(dim)])
+            for j in range(dim)]
+    rho0_jks = [[np.outer(ket1, ket2.conj()) for ket2 in kets]
+               for ket1 in kets]
+    soln_jks = [[get_non_herm_soln_time_ind(integrator, rho0, times, solve_ivp_kwargs) for rho0 in rho0_ks]
+                for rho0_ks in rho0_jks]
+    rhos_jks = [[soln.get_density_matrices() for soln in soln_ks]
+               for soln_ks in soln_jks]
+    return [np.array([[rhos[t_idx] for rhos in rhos_ks]
+                      for rhos_ks in rhos_jks])
+            for t_idx in range(len(times))]
 
 def make_encoding_process_tensor(kets):
     return np.array([[np.outer(ket1, ket2.conj()) for ket2 in kets]
