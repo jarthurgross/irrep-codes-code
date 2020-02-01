4631112040e17dbb9a875bc863372335954cc6d9
Jonathan A. Gross <jarthurgross@gmail.com>
2020-02-01 14:49:57 -0500

    Parallelize tensor construction from lindblads

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ requires = [
         'sympy',
         'picos',
         'cvxopt',
+        'joblib',
         'numpy-quaternion',
         'qinfo @ git+https://github.com/jarthurgross/numpy-quantum-info',
         'pysme @ git+https://git@github.com/CQuIC/pysme',
--- src/irrep_codes/sdp.py
+++ src/irrep_codes/sdp.py
@@ -1,28 +1,52 @@
 import numpy as np
 import picos as pic
 import cvxopt as cvx
+from joblib import Parallel, delayed
 
 import pysme.integrate as integ
 import qinfo.supops as supops
 
-def get_error_tensors(Ls, H, times, solve_ivp_kwargs=None):
+def get_error_tensors(Ls, H, times, solve_ivp_kwargs=None, n_jobs=1):
     integrator = integ.UncondLindbladIntegrator(Ls, H)
-    error_tensors = get_process_tensors_from_lindblad(integrator, times, solve_ivp_kwargs)
+    error_tensors = get_process_tensors_from_lindblad(integrator, times,
+                                                      solve_ivp_kwargs,
+                                                      n_jobs)
     return error_tensors
 
-def get_process_tensors_from_lindblad(integrator, times, solve_ivp_kwargs=None):
+def get_process_tensors_from_lindblad(integrator, times, solve_ivp_kwargs=None,
+        n_jobs=1):
     dim = integrator.basis.dim
     kets = [np.array([1 if j==k else 0 for k in range(dim)])
             for j in range(dim)]
     rho0_jks = [[np.outer(ket1, ket2.conj()) for ket2 in kets]
                for ket1 in kets]
-    soln_jks = [[get_non_herm_soln_time_ind(integrator, rho0, times, solve_ivp_kwargs) for rho0 in rho0_ks]
-                for rho0_ks in rho0_jks]
+    if n_jobs == 1:
+        soln_jks = [[get_non_herm_soln_time_ind(integrator, rho0, times,
+                                                solve_ivp_kwargs)
+                     for rho0 in rho0_ks]
+                    for rho0_ks in rho0_jks]
+    else:
+        soln_jks = Parallel(n_jobs=n_jobs)(delayed(get_non_herm_soln_time_ind)
+                                           (integrator, rho0, times,
+                                            soln_ivp_kwargs)
+                                           for rho0 in it.chain(*rho0_jks))
+        soln_jks = [soln_jks[dim*n:dim*(n+1)] for n in range(dim)]
     rhos_jks = [[soln.get_density_matrices() for soln in soln_ks]
                for soln_ks in soln_jks]
     return [np.array([[rhos[t_idx] for rhos in rhos_ks]
                       for rhos_ks in rhos_jks])
             for t_idx in range(len(times))]
+
+
+
+def get_non_herm_soln_time_ind(integrator, rho0, times, solve_ivp_kwargs):
+    rho0_vec = integrator.basis.vectorize(rho0, dense=True)
+    if np.linalg.norm(integrator.a_fn(times[0], rho0_vec)) < 1e-9:
+        print('Derivative is zero.')
+        return integ.Solution(np.repeat(rho0_vec[None,:], len(times), axis=0),
+                              integrator.basis.basis.todense())
+    else:
+        return integrator.integrate_non_herm(rho0, times, solve_ivp_kwargs)
 
 def make_encoding_process_tensor(kets):
     return np.array([[np.outer(ket1, ket2.conj()) for ket2 in kets]
