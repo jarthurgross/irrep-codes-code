7df6908ff67bb54872ffcf1333702a8a9eec3e22
Jonathan A. Gross <azaghalofbelegost@gmail.com>
2021-11-15 23:10:35 -0800

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup
 
 requires = [
-        'numpy',
-        'scipy',
-        'sympy',
-        'picos',
-        'cvxopt',
-        'joblib',
-        'numpy-quaternion',
-        'qinfo @ git+https://github.com/jarthurgross/numpy-quantum-info',
-        'pysme @ git+https://git@github.com/CQuIC/pysme@develop',
-         ]
+    'numpy',
+    'scipy',
+    'sympy',
+    'picos',
+    'cvxopt',
+    'joblib',
+    'numpy-quaternion',
+    'qinfo @ git+https://github.com/jarthurgross/numpy-quantum-info',
+    'pysme @ git+https://git@github.com/CQuIC/pysme@develop',
+]
 
-setup(name='irrep_codes',
-      version='0.1',
-      install_requires=requires,
-      packages=['irrep_codes'],
-      package_data={'': ['data/*.txt']},
-     )
+setup(
+    name='irrep_codes',
+    version='0.1',
+    install_requires=requires,
+    packages=['irrep_codes'],
+    package_data={'': ['data/*.txt']},
+)
